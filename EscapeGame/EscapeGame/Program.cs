7bf29cbff9a058ca45a784ac5fd36d2330705e80
Solvek <sa@mobiwolf.com>
2016-11-25 15:03:38 +0200

    Added 3d model

--- EscapeGame/EscapeGame/Program.cs
+++ EscapeGame/EscapeGame/Program.cs
@@ -2,8 +2,6 @@
 
 class Program
 {
-
-
     private int players = 2;
 
     public void BrainPadSetup()
@@ -19,44 +17,17 @@ class Program
     private void ShowSplash()
     {
         BrainPad.Display.Clear();
-        BrainPad.Display.DrawLargeText(13, 10, "Escape Game", BrainPad.Color.Magneta);
+        BrainPad.Display.DrawLargeText(13, 10, "Escape Game", BrainPad.Color.Cyan);
         BrainPad.Display.DrawText(30, 40, "by Nastia, Levko", BrainPad.Color.White);
         BrainPad.Display.DrawText(28, 50, "& Sergi Adamchuks", BrainPad.Color.White);
 
-        BrainPad.Display.DrawText(28, 90, "Press Any Button", BrainPad.Color.Cyan);
+        BrainPad.Display.DrawText(32, 90, "Press Any Button", BrainPad.Color.Red);
 
         var txtTemp = "Temperature: "+BrainPad.TemperatureSensor.ReadTemperature().ToString("F1")+" C";
 
-        BrainPad.Display.DrawText(28, 115, txtTemp, BrainPad.Color.Green);
+        BrainPad.Display.DrawText(27, 115, txtTemp, BrainPad.Color.Green);
 
-        var isButtonPressed = new Flag();
-        var setter = new BrainPad.Button.ButtonEventHandler((button, state) =>
-        {
-            isButtonPressed.Set();
-        });
-
-        BrainPad.Button.ButtonPressed += setter;
-
-        while(BrainPad.Looping)
-        {
-            BrainPad.TrafficLight.TurnRedLightOn();
-            BrainPad.Wait.Seconds(0.5);
-            if (isButtonPressed.IsSet()) break;
-            BrainPad.TrafficLight.TurnRedLightOff();
-            BrainPad.TrafficLight.TurnYellowLightOn();
-            BrainPad.Wait.Seconds(0.5);
-            if (isButtonPressed.IsSet()) break;
-            BrainPad.TrafficLight.TurnYellowLightOff();
-            BrainPad.TrafficLight.TurnGreenLightOn();
-            BrainPad.Wait.Seconds(0.5);
-            if (isButtonPressed.IsSet()) break;
-            BrainPad.TrafficLight.TurnGreenLightOff();
-        }
-
-        BrainPad.Button.ButtonPressed -= setter;
-        BrainPad.TrafficLight.TurnRedLightOff();
-        BrainPad.TrafficLight.TurnYellowLightOff();
-        BrainPad.TrafficLight.TurnGreenLightOff();
+        BlinkTraficUntilButtonPress();
 
         #if ENABLE_SOUND
         PlayMelody(10, new Tone[]{
@@ -78,22 +49,36 @@ class Program
         #endif
     }
 
-    /**
-     * Watches for an event to happen
-     **/
-    private class Flag
+    private static void BlinkTraficUntilButtonPress()
     {
-        private bool isSet = false;
-
-        public void Set()
+        var isButtonPressed = false;
+        var buttonPressMonitor = new BrainPad.Button.ButtonEventHandler((button, state) =>
         {
-            isSet = true;
+            isButtonPressed = true;
+        });
+
+        BrainPad.Button.ButtonPressed += buttonPressMonitor;
+
+        while (BrainPad.Looping)
+        {
+            BrainPad.TrafficLight.TurnRedLightOn();
+            BrainPad.Wait.Seconds(0.5);
+            if (isButtonPressed) break;
+            BrainPad.TrafficLight.TurnRedLightOff();
+            BrainPad.TrafficLight.TurnYellowLightOn();
+            BrainPad.Wait.Seconds(0.5);
+            if (isButtonPressed) break;
+            BrainPad.TrafficLight.TurnYellowLightOff();
+            BrainPad.TrafficLight.TurnGreenLightOn();
+            BrainPad.Wait.Seconds(0.5);
+            if (isButtonPressed) break;
+            BrainPad.TrafficLight.TurnGreenLightOff();
         }
 
-        public bool IsSet()
-        {
-            return isSet;
-        }
+        BrainPad.Button.ButtonPressed -= buttonPressMonitor;
+        BrainPad.TrafficLight.TurnRedLightOff();
+        BrainPad.TrafficLight.TurnYellowLightOff();
+        BrainPad.TrafficLight.TurnGreenLightOff();
     }
 
 
--- README.md
+++ README.md
@@ -6,3 +6,10 @@ A game with BrainPad board
 
  * [Board details](https://www.ghielectronics.com/support/brainpad)
  * [Getting started](https://www.ghielectronics.com/support/brainpad/duino)
+
+ # Game Board
+
+
+<img src="doc/Board.jpg" alt="Board Overview" width="400">
+
+[Sketchup Model](doc/Board.skp)
